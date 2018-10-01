d6900289ba5311dd0d2985b8401279d854de927c
patti-c <patricia.carter@flatironschool.com>
2018-10-01 16:26:14 -0400

    Further updated new user form

--- Taco_Project/app/controllers/users_controller.rb
+++ Taco_Project/app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
