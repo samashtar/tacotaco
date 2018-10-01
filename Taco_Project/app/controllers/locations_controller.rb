40db084300464b3b436286e07530abad97895b3e
patti-c <patricia.carter@flatironschool.com>
2018-10-01 18:13:11 -0400

    Built full CRUD for locations

--- Taco_Project/app/controllers/locations_controller.rb
+++ Taco_Project/app/controllers/locations_controller.rb
@@ -1,2 +1,63 @@
 class LocationsController < ApplicationController
+
+  before_action(:find_location, only: [:show, :edit, :update, :destroy])
+
+  def index
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      redirect_to new_location_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @location.update(location_params)
+      redirect_to location_path(@location)
+    else
+      redirect_to edit_location_path(@location)
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to locations_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(
+      :address,
+      :email,
+      :phone_number,
+      :name,
+      :opening_time,
+      :closing_time)
+  end
+
+  def find_location
+    @location = Location.find(params[:id])
+  end
+
+  def require_admin
+
+  end
+
 end
