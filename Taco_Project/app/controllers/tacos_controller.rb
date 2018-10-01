59b8922ae8d67d87634f8983d44b7c62d6e2d54d
samashtar <sam.ashtar@flatironschool.com>
2018-10-01 14:17:41 -0400

    new taco form

--- Taco_Project/app/controllers/tacos_controller.rb
+++ Taco_Project/app/controllers/tacos_controller.rb
@@ -1,2 +1,47 @@
 class TacosController < ApplicationController
+
+def new
+  @taco = Taco.new
+end
+
+def create
+@taco = Taco.create(ingredient_ids:params[:ingredients][:ingredient_id].compact)
+  if
+  @taco.valid?
+  redirect_to @taco
+  else
+  redirect_to :new
+end
+end
+
+def show
+  @taco = Taco.find(params)
+end
+
+
+
+def edit
+end
+
+def update
+
+end
+
+def destroy
+end
+
+
+
+
+private
+
+def taco_params
+end
+
+
+
+
+
+
+
 end
