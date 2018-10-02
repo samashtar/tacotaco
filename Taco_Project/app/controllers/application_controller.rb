38565580d191a689f270c9a4791888133b9e4d80
patti-c <patricia.carter@flatironschool.com>
2018-10-02 09:57:52 -0400

    Improved login flow

--- Taco_Project/app/controllers/application_controller.rb
+++ Taco_Project/app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def index
-    
+
   end
 
   def current_user
-    if session[:user_id]
+    if !session[:user_id].nil?
       User.find(session[:user_id])
     else
-      redirect_to login_path and return
+      nil
     end
   end
 
