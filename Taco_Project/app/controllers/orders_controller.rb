1518b2ff25ec7d959b9a641c0f5ea62f41cb07d4
patti-c <patricia.carter@flatironschool.com>
2018-10-03 10:28:01 -0400

    Fixed some spacing

--- Taco_Project/app/controllers/orders_controller.rb
+++ Taco_Project/app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
-def index
-  @user = current_user
-end
+  
+  def index
+    @user = current_user
+  end
 
-def finalize
-  @user = current_user
-end
+  def finalize
+    @user = current_user
+  end
 
 end
