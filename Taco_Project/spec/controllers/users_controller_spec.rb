f245ec6c3365bbb2434148f4473dd32b10b77de4
patti-c <patricia.carter@flatironschool.com>
2018-10-04 17:08:55 -0400

    removed custom error stuff

---
+++ Taco_Project/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,29 @@
+# require File.expand_path('../../../config/environment', __FILE__)
+# require 'rspec-rails'
+#
+# describe UsersController, type: :controller do
+#
+#   let(:attributes) do
+#     {
+#     name: "Test User",
+#     username: "testusername",
+#     email: "testuser@testemail.com",
+#     address: "404 Test Lane",
+#     phone_number: 5555555555,
+#     location_id: Location.first.id,
+#     taco_points: 0,
+#     password: "testuserpassword"
+#     }
+#   end
+#
+#   describe "creation" do
+#     before { post :create, params:{user: attributes} }
+#     let(:user) {User.find_by(name: "Test User")}
+#
+#     it "creates a new user" do
+#       expect(user).to_not be_nil
+#     end
+#
+#   end
+#
+# end
